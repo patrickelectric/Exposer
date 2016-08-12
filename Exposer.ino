123b90db745754944e5af7fe2071315055ee574d
Willian Galvani <williangalvani@gmail.com>
2016-08-12 18:21:54 -0300

    Improve debug on the arduino side, now it ouputs all the values every second

--- Exposer.ino
+++ Exposer.ino
@@ -12,6 +12,8 @@ int16_t testint16 = 0;
 int32_t testint32 = 0;
 float testfloat = 0;
 
+long next;
+
 void setup()
 {
     Serial.begin(115200);
@@ -20,20 +22,35 @@ void setup()
     exposer->registerVariable(VARNAME(testuint8), Exposer::_uint8_t, &testuint8);
     exposer->registerVariable(VARNAME(testuint16), Exposer::_uint16_t, &testuint16);
     exposer->registerVariable(VARNAME(testuint32), Exposer::_uint32_t, &testuint32);
-    /*exposer->registerVariable(VARNAME(testint8), Exposer::_int8_t, &testint8);
+    exposer->registerVariable(VARNAME(testint8), Exposer::_int8_t, &testint8);
     exposer->registerVariable(VARNAME(testint16), Exposer::_int16_t, &testint16);
     exposer->registerVariable(VARNAME(testint32), Exposer::_int32_t, &testint32);
-    exposer->registerVariable(VARNAME(testfloat), Exposer::_float, &testfloat);*/
+    exposer->registerVariable(VARNAME(testfloat), Exposer::_float, &testfloat);
+
+
+    next = millis()+1000;
 }
 
 void loop()
 {
-    digitalWrite(13,(led>100));
+
+    digitalWrite(13,(testint8==-50));
 
     if (Serial.available())
     {
         uint8_t data = Serial.read();
         exposer->processByte(data);
     }
+    if (millis()>next){
+        Serial.print("uint8:");Serial.println(testuint8);
+        Serial.print("uint16:");Serial.println(testuint16);
+        Serial.print("uint32:");Serial.println(testuint32);
+        Serial.print("int8:");Serial.println(testint8);
+        Serial.print("int16:");Serial.println(testint16);
+        Serial.print("int32:");Serial.println(testint32);
+        Serial.print("float:");Serial.println(testfloat);
+        next = millis()+1000;
+    }
+
 }
 
