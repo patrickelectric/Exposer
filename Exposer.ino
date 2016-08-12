cfeb790a8b6bf7cb4a5a24dfaf64247dc9117275
Willian Galvani <williangalvani@gmail.com>
2016-08-12 12:57:52 -0300

    add new variables uint16, uint32, for testint purposes

--- Exposer.ino
+++ Exposer.ino
@@ -4,11 +4,26 @@ Exposer* exposer = &Exposer::self();
 
 uint8_t led = 0;
 
+uint8_t testuint8 = 0;
+uint16_t testuint16 = 0;
+uint32_t testuint32 = 0;
+int8_t testint8 = 0;
+int16_t testint16 = 0;
+int32_t testint32 = 0;
+float testfloat = 0;
+
 void setup()
 {
     Serial.begin(115200);
     pinMode(13, OUTPUT);
     exposer->registerVariable(VARNAME(led), Exposer::_uint8_t, &led);
+    exposer->registerVariable(VARNAME(testuint8), Exposer::_uint8_t, &testuint8);
+    exposer->registerVariable(VARNAME(testuint16), Exposer::_uint16_t, &testuint16);
+    exposer->registerVariable(VARNAME(testuint32), Exposer::_uint32_t, &testuint32);
+    /*exposer->registerVariable(VARNAME(testint8), Exposer::_int8_t, &testint8);
+    exposer->registerVariable(VARNAME(testint16), Exposer::_int16_t, &testint16);
+    exposer->registerVariable(VARNAME(testint32), Exposer::_int32_t, &testint32);
+    exposer->registerVariable(VARNAME(testfloat), Exposer::_float, &testfloat);*/
 }
 
 void loop()
